d96254d20becd515de0556c2e37eb49c3e0551d0
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-08-05 17:55:04 +0000

    mejoras

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const icons = document.querySelectorAll(".icons");
+  const numberToShow = document.querySelector(".numberToShow");
+  const button = document.querySelector("#button");
+  const Nramdon = Math.random();
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const symbols = [
+    { clase: "heart text-danger", icon: "♥" },
+    { clase: "diamond", icon: "♦" },
+    { clase: "club", icon: "♣" },
+    { clase: "spade", icon: "♠" }
+  ];
+  icons.forEach(i => {
+    i.className += ` ${
+      symbols[Math.floor(Nramdon * symbols.length)].clase
+    } fs-1`;
+    i.textContent = ` ${symbols[Math.floor(Nramdon * symbols.length)].icon}`;
+  });
+  numberToShow.textContent = numbers[Math.floor(Nramdon * numbers.length)];
 };
